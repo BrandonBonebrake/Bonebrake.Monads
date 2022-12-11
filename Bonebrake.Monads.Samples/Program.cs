c3e5a884795c329af223d2053235f88122241f11
Brandon Bonebrake <bbonebrake67@gmail.com>
2022-12-11 01:33:24 -0600

    Adding console app which can demo the monads

---
+++ Bonebrake.Monads.Samples/Program.cs
@@ -0,0 +1,32 @@
+﻿using Bonebrake.Monads;
+
+"Hello World"
+	.OfMaybe()
+	.Invoke(Console.WriteLine);
+
+"Hello World 2"
+	.OkResult()
+	.InvokeErrors(x => Console.WriteLine("This will not be in the console since the result is ok"))
+	.Bind(x => x.Length)
+	.Invoke(Console.WriteLine);
+	
+"Errored Message"
+	.OfResultError()
+	.FailureResult<string>()
+	.InvokeErrors(x =>
+	{
+		foreach (var resultError in x)
+		{
+			Console.WriteLine(resultError);
+		}
+	})
+	.Invoke(x => Console.WriteLine("This will not be in the console since the result errored"));
+	
+"Hello World 3"
+	.OkResult()
+	.Bind(x => x)
+	.Unbind()
+	.Unbind()
+	.OfMaybe()
+	.Invoke(Console.WriteLine);
+	
--- Bonebrake.Monads/ResultError.cs
+++ Bonebrake.Monads/ResultError.cs
@@ -1,3 +1,8 @@
 ﻿namespace Bonebrake.Monads;
 
 public readonly record struct ResultError(string Message);
+
+public static class ResultErrorExt
+{
+	public static ResultError OfResultError(this string message) => new(message);
+}
